24167ec14d21e3cdbec077cdbe7e9fc72f822577
Lelièvre <63064871+sylvainlelievre@users.noreply.github.com>
2020-06-14 08:33:55 +0200

    Version 2.7

--- agenda.php
+++ agenda.php
@@ -12,7 +12,7 @@
  * @link http://zwiicms.com/
  */
  /** Module Zwii développé par Sylvain Lelièvre
- * Utilise le package Fullcalendar 
+ * Utilise le package Fullcalendar
  * FullCalendar Core Package v4.3.1
  * Docs & License: https://fullcalendar.io/
  * (c) 2019 Adam Shaw
@@ -30,7 +30,7 @@ class agenda extends common {
 	];
 
 	const AGENDA_VERSION = '2.7';
-	
+
 	//Couleur du bandeau et du texte
 	public static $couleur = [
 		'black' => 'noir',
@@ -42,14 +42,14 @@ class agenda extends common {
 		'green' => 'vert',
 		'white' => 'blanc'
 	];
-	
+
 	public static $groupe = [
 		'0' => 'Visiteur',
 		'1' => 'Membre',
 		'2' => 'Editeur',
 		'3' => 'Administrateur'
 	];
-	
+
 	//Evenement
 	public static $evenement = [
 		'id' => 0,
@@ -61,7 +61,7 @@ class agenda extends common {
 		'groupe_lire' => 0,
 		'groupe_mod' => 2
 	];
-	
+
 	//Largeur maximale de l'agenda
 	public static $maxwidth = [
 		'400' => '400 pixels',
@@ -73,21 +73,21 @@ class agenda extends common {
 		'1130' => '1130 pixels',
 		'10000' => '100%'
 	];
-	
+
 	public static $datecreation = '';
-	
+
 	//Pour choix de l'affichage mois / semaine dans configuration de l'agenda
 	public static $vue_agenda = [
 		'dayGridMonth' => 'Vue par mois',
 		'dayGridWeek' => 'Vue par semaine'
 	];
 
-	
+
 	/**
 	 * Configuration Paramètrage
 	 */
 	public function config() {
-		
+
 		// Soumission du formulaire
 		if($this->isPost()) {
 			$fichier_restaure = $this->getInput('config_restaure');
@@ -95,32 +95,32 @@ class agenda extends common {
 			$droit_creation = $this->getInput('config_droit_creation');
 			$droit_limite = $this->getInput('config_droit_limite', helper::FILTER_BOOLEAN);
 			$fichier_ics = $this->getInput('config_fichier_ics');
-			$largeur_maxi = $this->getInput('config_MaxiWidth'); 
-				
+			$largeur_maxi = $this->getInput('config_MaxiWidth');
+
 			//Sauvegarder l'agenda
 			if ($fichier_sauve !=''){
 				$json_sauve = file_get_contents('module/agenda/data/'.$this->getUrl(0).'/events.json');
 				file_put_contents('module/agenda/data/'.$this->getUrl(0).'_sauve/'.$fichier_sauve.'.json', $json_sauve);
 			}
-			
+
 			//Charger un agenda sauvegardé
 			if (strpos($fichier_restaure,'.json') !== false){
-								
+
 				//Remplacement par le fichier de restauration
 				$json_restaure = file_get_contents('module/agenda/data/'.$this->getUrl(0).'_sauve/'. $fichier_restaure);
 				file_put_contents('module/agenda/data/'.$this->getUrl(0).'/events.json', $json_restaure);
-				
+
 				//Sauvegarde dans data_sauve de l'agenda chargé
 				$this->sauve($json_restaure);
-				
-				//Valeurs en sortie après prise en compte du formulaire 
+
+				//Valeurs en sortie après prise en compte du formulaire
 				$this->addOutput([
 					'notification' => 'Agenda chargé',
 					'redirect' => helper::baseUrl() . $this->getUrl(0),
 					'state' => true
 				]);
 			}
-			
+
 			//Ajouter des évènements contenus dans le fichier ics
 			if (strpos($fichier_ics,'.ics') !== false){
 				$tableau = $this->getIcsEventsAsArray('./site/file/source/agenda/ics/'.$fichier_ics);
@@ -150,31 +150,31 @@ class agenda extends common {
 							$end = $value2;
 						}
 					}
-					
+
 					//Si un évènement VEVENT est trouvé, avec summary et dtstart présents, on ajoute cet évènement à l'agenda
 					if ($evenement_texte != '' && strpos($begin,'VEVENT')!==false && $date_debut!=='' ){
 						if($date_fin == '') {
-							$date_fin = $date_debut; 
+							$date_fin = $date_debut;
 							$clef_fin = $clef_debut;
 						}
 						$evenement_texte = $this->modif_texte($evenement_texte);
 						//Modifier date format ics yyyymmddThhmm... ou yyyymmdd vers format fullcalendar yyyy-mm-ddThh:mm
 						$date_debut = $this->modif_date($date_debut, $clef_debut);
 						$date_fin = $this->modif_date($date_fin, $clef_fin);
-					
+
 						//Valeurs par défaut pour l'import ics fond blanc, texte noir, lecture visiteur, modification éditeur
-						$this->nouvel_evenement($evenement_texte,$date_debut,$date_fin,'white','black','0','2','');			
+						$this->nouvel_evenement($evenement_texte,$date_debut,$date_fin,'white','black','0','2','');
 					}
 				}
 			}
-			
+
 			//Mise à jour des données de configuration liées aux droits et à l'affichage
 			$this->setData(['module', $this->getUrl(0), 'config', [
 				'droit_creation' => intval($droit_creation),
 				'droit_limite' => $droit_limite,
 				'maxiWidth' => $largeur_maxi
 			]]);
-		
+
 			//Valeurs en sortie
 			$this->addOutput([
 				'notification' => 'Opérations enregistrées',
@@ -191,14 +191,14 @@ class agenda extends common {
 			]);
 		}
 	}
-	
-	
-	
+
+
+
 	/**
 	 * Suppression d'un évènement
 	 */
 	public function delete($lid, $sauve, $json) {
-	
+
 		//$pos1 et $pos2 sont les délimiteurs de la partie à supprimer
 		$pos1 = strpos($json, '{"id":'.$lid);
 		$pos2 = strpos($json, '}', $pos1);
@@ -215,15 +215,15 @@ class agenda extends common {
 		else{
 			$json = substr_replace($json,',{}',$pos1-1, $pos2-$pos1+2);
 		}
-		
+
 		//Enregistrer le nouveau fichier json
 		//file_put_contents('module/agenda/data/'.$this->getUrl(0).'/events.json', $json);
-		
+
 		//Enregistrer le json et sauvegarder dans data_sauve si suppression de l'évènement et non modification
 		if ($sauve == true){
 			file_put_contents('module/agenda/data/'.$this->getUrl(0).'/events.json', $json);
 			$this->sauve($json);
-				
+
 			//Valeurs en sortie si suppression demandée et réalisée
 			$this->addOutput([
 					'notification' => 'Evènement supprimé',
@@ -235,38 +235,38 @@ class agenda extends common {
 			return $json;
 		}
 	}
-	
-	
+
+
 	/**
 	 * Suppression de tous les évènements
 	 */
 	public function deleteall() {
-	
+
 		//Sauvegarde dans data de l'agenda actuel bien qu'il soit déjà sauvegardé dans data_sauve
 		$json = file_get_contents('module/agenda/data/'.$this->getUrl(0).'/events.json');
 		file_put_contents('module/agenda/data/'.$this->getUrl(0).'/events_'.date('YmdHis').'.json', $json);
-		
+
 		//Enregistrer le nouveau fichier json vide
-		$json='[]';	
+		$json='[]';
 		file_put_contents('module/agenda/data/'.$this->getUrl(0).'/events.json', $json);
-		
+
 		//Valeurs en sortie
 		$this->addOutput([
 				'notification' => 'Suppression de tous les évènements',
 				'redirect' => helper::baseUrl() . $this->getUrl(0),
 				'state' => true
 		]);
-	
+
 	}
-	
+
 	/**
 	 * Création
 	 */
 	public function creation() {
-	
+
 		// Soumission du formulaire
 		if($this->isPost()) {
-		
+
 			//lecture du formulaire
 			$evenement_texte = $this->getInput('creation_text',null);
 			$date_debut = $this->getInput('creation_date_debut');
@@ -278,20 +278,20 @@ class agenda extends common {
 
 			//Modification de CR LF " { } dans le texte de l'évènement
 			$evenement_texte = $this->modif_texte($evenement_texte);
-		
-			//Vérification que date fin > date debut			
-			if ($this->verif_date($date_debut,$date_fin)){ 
-			
+
+			//Vérification que date fin > date debut
+			if ($this->verif_date($date_debut,$date_fin)){
+
 				//Ajout et enregistrement de l'évènement
 				$json = file_get_contents('module/agenda/data/'.$this->getUrl(0).'/events.json');
 				$this->nouvel_evenement($evenement_texte,$date_debut,$date_fin,$couleur_fond,$couleur_texte,$groupe_visible,$groupe_mod,$json);
-		
+
 				//Valeurs en sortie après prise en compte du formulaire
 				$this->addOutput([
 					'notification' => 'Evènement enregistré',
 					'state' => true,
 					'redirect' => helper::baseUrl() . $this->getUrl(0)
-				]);			
+				]);
 			}
 			//Valeurs saisies non correctes
 			else{
@@ -299,7 +299,7 @@ class agenda extends common {
 					'notification' => 'La date de fin précède la date de début !',
 					'view' => 'creation',
 					'state' => false
-				]);		
+				]);
 			}
 		}
 		else{
@@ -312,12 +312,12 @@ class agenda extends common {
 			]);
 		}
 	}
-	
+
 	/**
 	 * Edition, modification, suppression
 	 */
 	public function edition($lid) {
-	
+
 		//Préparation avant l'édition de l'évènement
 		self::$evenement['id'] = $lid;
 		$json = file_get_contents('module/agenda/data/'.$this->getUrl(0).'/events.json');
@@ -326,15 +326,15 @@ class agenda extends common {
 		self::$evenement['groupe_mod'] = $tableau[$lid]['groupe_mod'];
 		self::$evenement['texte'] = $this->restaure_texte($tableau[$lid]['title']);
 		self::$evenement['couleurfond'] = $tableau[$lid]['backgroundColor'];
-		self::$evenement['couleurtexte'] = $tableau[$lid]['textColor'];	
+		self::$evenement['couleurtexte'] = $tableau[$lid]['textColor'];
 		$dateclic = $tableau[$lid]['start'];
-		self::$evenement['datedebut'] = $this->conversion_date($dateclic);	
+		self::$evenement['datedebut'] = $this->conversion_date($dateclic);
 		$dateclic = $tableau[$lid]['end'];
 		self::$evenement['datefin'] = $this->conversion_date($dateclic);
-		
+
 		//Soumission du formulaire
 		if($this->isPost()) {
-		
+
 			//Si bouton submit enregistrer
 			if (isset($_POST['enregistrer'])) {
 				//lecture du formulaire
@@ -348,14 +348,14 @@ class agenda extends common {
 
 				//Modification de CR LF " { } dans le texte de l'évènement
 				$evenement_texte = $this->modif_texte($evenement_texte);
-			
-				//Vérification que date fin > date debut			
-				if ($this->verif_date($date_debut,$date_fin)){ 
-				
+
+				//Vérification que date fin > date debut
+				if ($this->verif_date($date_debut,$date_fin)){
+
 					//Effacer l'évènement sans sauvegarde dans data_sauve
 					$sauve = false;
 					$json = $this->delete($lid, $sauve, $json);
-					
+
 					//Ajout, enregistrement et sauvegarde de l'évènement
 					$this->nouvel_evenement($evenement_texte,$date_debut,$date_fin,$couleur_fond,$couleur_texte,$groupe_visible,$groupe_mod,$json);
 
@@ -373,13 +373,13 @@ class agenda extends common {
 						'notification' => 'La date de fin précède la date de début !',
 						'view' => 'edition',
 						'state' => false
-					]);				
+					]);
 				}
-  
+
 			// sinon si bouton submit supprimer
 			} elseif (isset($_POST['supprimer'])) {
 				$sauve = true;
-				$this->delete($lid, $sauve, $json); 
+				$this->delete($lid, $sauve, $json);
 			}
 		}
 		else{
@@ -393,20 +393,20 @@ class agenda extends common {
 		}
 	}
 
-	
+
 	/**
 	 * Newname utilisé par la version 9 pour inscrire le nouveau nom de page dans le json du module
 	 */
 	 public function newname() {
 			$this->setData(['module',$this->getUrl(0),'name',$this->getUrl(0)]);
 	 }
-	 
+
 	/**
 	 * Accueil
 	 */
 	public function index() {
 
-		//Pour récupération des données ajax jquery date ou id 
+		//Pour récupération des données ajax jquery date ou id
 		$url = $_SERVER['REQUEST_URI'];
 		if (strpos($url,'/da:') !== false){
 			//Extraction des données de la chaîne url et détection de changement de vue
@@ -416,7 +416,7 @@ class agenda extends common {
 			$this->creation();
 		}
 		else{
-			if (strpos($url,'/id:') !== false){	
+			if (strpos($url,'/id:') !== false){
 				//Extraction des données de la chaîne url et détection de changement de vue
 				$idclic = $this->vue_debut($url,'/id:');
 				//Vers l'édition d'un évènement
@@ -438,7 +438,7 @@ class agenda extends common {
 						'maxiWidth' => '800'
 						]
 					]]);
-				
+
 					//Création des dossiers de sauvegarde de l'agenda
 					if(! is_dir('./module/agenda/data')){mkdir('./module/agenda/data');}
 					if(! is_dir('./module/agenda/data/'.$this->getUrl(0).'_sauve')){mkdir('./module/agenda/data/'.$this->getUrl(0).'_sauve');}
@@ -447,11 +447,11 @@ class agenda extends common {
 					if(! is_dir('./site/file/source/agenda')){mkdir('./site/file/source/agenda');}
 					if(! is_dir('./site/file/source/agenda/ics')){mkdir('./site/file/source/agenda/ics');}
 
-				
+
 					$this->addOutput([
 							'notification' => 'Initialisations effectuées',
 							'state' => true
-					]);	
+					]);
 				}
 				else{
 					//le module existe dans le json, détection du changement de nom de la page pour renommer les dossiers
@@ -460,24 +460,24 @@ class agenda extends common {
 						$newname = $this->getUrl(0);
 						rename( './module/agenda/data/'.$oldname, './module/agenda/data/'.$newname);
 						rename( './module/agenda/data/'.$oldname.'_visible' , './module/agenda/data/'.$newname.'_visible');
-						rename( './module/agenda/data/'.$oldname.'_sauve' , './module/agenda/data/'.$newname.'_sauve');	
+						rename( './module/agenda/data/'.$oldname.'_sauve' , './module/agenda/data/'.$newname.'_sauve');
 						$this->addOutput([
 								'notification' => 'Modification des dossiers de sauvegarde',
 								'state' => true
-						]);	
+						]);
 						//Fonctionne avec Zwii 10.0.044 mais sans effet avec version 9.2.27, pourquoi ?
-						//$this->setData(['module',$newname,'name',$newname]);	
+						//$this->setData(['module',$newname,'name',$newname]);
 						//avec une version 9 on passe par une fonction pour réaliser cette mise à jour
 						$this->newname();
-			
+
 					}
-				}	
+				}
 				//Si le fichier events.json n'existe pas ou si sa taille est inférieure à 2 on le crée vide
-				if( is_file('module/agenda/data/'.$this->getUrl(0).'/events.json') === false || 
+				if( is_file('module/agenda/data/'.$this->getUrl(0).'/events.json') === false ||
 				( is_file('module/agenda/data/'.$this->getUrl(0).'/events.json') === true && filesize('module/agenda/data/'.$this->getUrl(0).'/events.json')<2)){
 					file_put_contents('module/agenda/data/'.$this->getUrl(0).'/events.json', '[]');
 				}
-				
+
 				//Création d'une copie d'events.json visible en fonction des droits
 				$json = file_get_contents('module/agenda/data/'.$this->getUrl(0).'/events.json');
 				$tableau = json_decode($json, true);
@@ -492,13 +492,13 @@ class agenda extends common {
 								$newvalue = strip_tags($newvalues);
 								//Modification de CR LF " { } dans le texte de l'évènement
 								$newvalue = $this->modif_texte($newvalue);
-								$json = str_replace($value['title'], $newvalue, $json); 
+								$json = str_replace($value['title'], $newvalue, $json);
 							}
 						}
 					}
 				}
 				file_put_contents('module/agenda/data/'.$this->getUrl(0).'_visible/events.json',$json);
-				
+
 				// Affichage de la page agenda
 				$this->addOutput([
 					'showBarEditButton' => true,
@@ -508,17 +508,17 @@ class agenda extends common {
 					],
 					'view' => 'index'
 				]);
-			
+
 			}
 		}
 
 	}
-	
 
-	/* 
+
+	/*
 	/*Fonctions privées
 	*/
-	
+
 	/* Conversion date au format unix (valeur 0 au 1/1/1970 00:00)
 	*/
 	private function conversion_date($dateclic){
@@ -532,8 +532,8 @@ class agenda extends common {
 		$date->setTime($heure, $minute);
 		return $date->getTimestamp();
 	}
-	
-	
+
+
 	/* Vérification que $datedebut précède $datefin
 	*/
 	private function verif_date($datedebut, $datefin){
@@ -551,7 +551,7 @@ class agenda extends common {
 		if ($valdate[0] <= $valdate[1]){ $result = true;}
 		return $result;
 	}
-	
+
 	/*Modifier date format ics yyyymmddThhmm...  ou yyyymmdd vers format fullcalendar yyyy-mm-ddThh:mm ou yyyy-mm-dd
 	*/
 	private function modif_date($datein, $clef){
@@ -563,9 +563,9 @@ class agenda extends common {
 		}
 		return $dateout;
 	}
-						
-		
-	
+
+
+
 	/* Modification de CR LF " ' { } dans le texte de l'évènement
 	*/
 	private function modif_texte($evenement_texte){
@@ -577,7 +577,7 @@ class agenda extends common {
 		$evenement_texte = str_replace('{','&#123;',$evenement_texte);
 		return $evenement_texte;
 	}
-	
+
 	/* Restauration des CR LF " ' { } dans le texte de l'évènement
 	*/
 	private function restaure_texte($evenement_texte){
@@ -589,23 +589,23 @@ class agenda extends common {
 		$evenement_texte = str_replace('&#123;','{',$evenement_texte);
 		return $evenement_texte;
 	}
-	
+
 	/* Ajout et enregistrement d'un évènement sur création ou édition
 	*/
 	private function nouvel_evenement($evenement_texte,$date_debut,$date_fin,$couleur_fond,$couleur_texte,$groupe_visible,$groupe_mod,$json){
 		//Changement du format des dates yyyy-mm-dd hh:mm:0  vers format fullcalendar yyyy-mm-ddThh:mm
 		$date_debut = str_replace(' ','T',$date_debut);
 		$date_fin = str_replace(' ','T',$date_fin);
-		
+
 		//Limitation à 16 caractères
 		$date_debut = substr($date_debut,0,16);
 		$date_fin = substr($date_fin,0,16);
-		
+
 		//Ouverture et décodage du fichier json
 		if($json == ''){$json = file_get_contents('module/agenda/data/'.$this->getUrl(0).'/events.json');}
 		$tableau = json_decode($json);
 		$keynew = count($tableau);
-		
+
 		//Chaîne à ajouter de type ,{"id":"2","title":"...","start":"...","end":"...","backgroundColor":"...","textColor":"...","groupe":"..."}]
 		//Sans la virgule initiale si c'est le premier évènement
 		if (strlen($json) > 2){
@@ -622,7 +622,7 @@ class agenda extends common {
 		file_put_contents('module/agenda/data/'.$this->getUrl(0).'/events.json', $json);
 		$this->sauve($json);
 	}
-	
+
 	/* Sauvegarde automatique de l'agenda sous une forme datée après chaque création, modification, suppression d'un évènement
 	* ou chargement d'un nouvel agenda, seuls les 10 derniers agendas sont sauvegardés
 	*/
@@ -630,7 +630,7 @@ class agenda extends common {
 
 		//Sauvegarde du fichier json actuel
 		file_put_contents('module/agenda/data/'.$this->getUrl(0).'_sauve/events_'.date('YmdHis').'.json', $sauve_json);
-			
+
 		//Effacement du plus ancien fichier de sauvegarde auto si le nombre de fichiers dépasse 10
 		$dir='./module/agenda/data/'.$this->getUrl(0).'_sauve';
 		$nom_fichier = scandir($dir);
@@ -649,9 +649,9 @@ class agenda extends common {
 			closedir($handle);
 		}
 	}
-	
+
 	/* Suppression d'évènements dans le json public ( visible) en fonction des droits
-	*/ 
+	*/
 	private function delete_visible($json,$lid) {
 		//$pos1 et $pos2 sont les délimiteurs de la partie à supprimer
 		$pos1 = strpos($json, '{"id":'.$lid);
@@ -673,7 +673,7 @@ class agenda extends common {
 	}
 
 	/* Limitation des choix pour les groupes lecture et modification avant création ou édition
-	*/ 
+	*/
 	private function limite_groupes() {
 		//Modification du tableau self::$groupe si case cochée en configuration
 		if ($this->getData(['module', $this->getUrl(0), 'config', 'droit_limite'])
@@ -691,7 +691,7 @@ class agenda extends common {
 			 }
 		}
 	}
-	
+
 	/*
 	* Extraction des données de la chaîne url et détection de changement de vue
 	*/
@@ -708,15 +708,15 @@ class agenda extends common {
 			$this->setData(['module', $this->getUrl(0), 'vue', [
 				'vueagenda' => $grid,
 				'debagenda' => $deb
-				]]);		
+				]]);
 			$this->addOutput([
 				'notification' => 'Modification de vue enregistrée',
 				'state' => true
-			]);	
+			]);
 		}
 		return $iddaclic;
 	}
-	
+
 	/* Function is to get all the contents from ics and explode all the datas according to the events and its sections */
 	/* de https://www.apptha.com/blog/import-google-calendar-events-in-php/ */
     function getIcsEventsAsArray($file) {
@@ -737,9 +737,9 @@ class agenda extends common {
         }
         return $icsDates;
     }
-	
+
     /* funcion is to avaid the elements wich is not having the proper start, end  and summary informations */
-	/* de https://www.apptha.com/blog/import-google-calendar-events-in-php/ */	
+	/* de https://www.apptha.com/blog/import-google-calendar-events-in-php/ */
     function getICSDates($key, $subKey, $subValue, $icsDates) {
         if ($key != 0 && $subKey == 0) {
             $icsDates [$key] ["BEGIN"] = $subValue;
